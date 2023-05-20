3a30e9a32e0783d2b98875fb97c2b89d7e150c9a
Babatunde Joseph <babatundejoseph85@gmail.com>
2023-05-20 16:52:04 +0100

    setup the app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className="italic container mx-auto text-slate-600 h-screen flex justify-center items-center">
+      <h1 className="text-5xl">
+        React & Vite & Tailwindcss
+      </h1>
+    </div>
+  )
+}
+
+export default App
